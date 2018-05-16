f71beb26e48e1e8c2ca940cf0c5931182f072e01
SMHendryx <seanmhendryx@gmail.com>
2018-05-15 17:07:34 -0700

    updates

--- Bash/listDirectorySizes.sh
+++ Bash/listDirectorySizes.sh
@@ -1,3 +1,2 @@
-#lists the size of the directory and all its subdirectories
-# TODO: need to figure out how to list SPECIFIC directory sizes, i.e. take a string of directory path and return the cumulative size of subdirectory contents 
-ssh v11 "df -h"
+#lists the size of the subdirectories and their contents
+ssh v11 "du -sh /data/faces/neonate_pain/*"
